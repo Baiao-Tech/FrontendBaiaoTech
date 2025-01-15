a51ceb8369cd91776186feee1bef635f9fe5a718
mario-leandro <marioleandro2004@gmail.com>
2025-01-15 14:52:17 -0300

    Mantendo as categorias em um array

---
+++ src/components/categorias.ts
@@ -0,0 +1,42 @@
+export const categorias = [
+    {
+        id: 1,
+        nome: "Front-end",
+    },
+    {
+        id: 2,
+        nome: "Back-end",
+    },
+    {
+        id: 3,
+        nome: "Mobile",
+    },
+    {
+        id: 4,
+        nome: "DevOps",
+    },
+    {
+        id: 5,
+        nome: "Gestão",
+    },
+    {
+        id: 6,
+        nome: "Marketing",
+    },
+    {
+        id: 7,
+        nome: "Inovação & Gestão",
+    },
+    {
+        id: 8,
+        nome: "UX & UI",
+    },
+    {
+        id: 9,
+        nome: "Data Science",
+    },
+    {
+        id: 10,
+        nome: "IA",
+    },
+]
